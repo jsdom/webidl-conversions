4619e36f8318088632a1aeb47adf8bfbd9cc3042
Domenic Denicola <domenic@domenicdenicola.com>
2014-10-13 17:51:20 -0400

    Use more ES6 in the tests

--- test/boolean.js
+++ test/boolean.js
@@ -5,20 +5,20 @@ import conversions from "..";
 describe("WebIDL boolean type", () => {
     var sut = conversions["boolean"];
 
-    it("should return `false` for `undefined`", function () {
+    it("should return `false` for `undefined`", () => {
         assert.strictEqual(sut(undefined), false);
     });
 
-    it("should return `false` for `null`", function () {
+    it("should return `false` for `null`", () => {
         assert.strictEqual(sut(null), false);
     });
 
-    it("should return the input for a boolean", function () {
+    it("should return the input for a boolean", () => {
         assert.strictEqual(sut(true), true);
         assert.strictEqual(sut(false), false);
     });
 
-    it("should return `false` for `+0`, `-0`, and `NaN`, but `true` other numbers", function () {
+    it("should return `false` for `+0`, `-0`, and `NaN`, but `true` other numbers", () => {
         assert.strictEqual(sut(+0), false);
         assert.strictEqual(sut(-0), false);
         assert.strictEqual(sut(NaN), false);
@@ -27,20 +27,20 @@ describe("WebIDL boolean type", () => {
         assert.strictEqual(sut(-Infinity), true);
     });
 
-    it("should return `false` for empty strings, but `true` for other strings", function () {
+    it("should return `false` for empty strings, but `true` for other strings", () => {
         assert.strictEqual(sut(""), false);
         assert.strictEqual(sut(" "), true);
         assert.strictEqual(sut("false"), true);
     });
 
-    it("should return `true` for symbols", function () {
+    it("should return `true` for symbols", () => {
         assert.strictEqual(sut(Symbol()), true);
     });
 
-    it("should return `true` for objects", function () {
+    it("should return `true` for objects", () => {
         assert.strictEqual(sut({}), true);
         assert.strictEqual(sut(Object.create(null)), true);
-        assert.strictEqual(sut(function () { }), true);
+        assert.strictEqual(sut(() => { }), true);
         assert.strictEqual(sut(new Boolean(false)), true);
         assert.strictEqual(sut(new Number(0)), true);
     });
--- test/dom-string.js
+++ test/dom-string.js
@@ -5,23 +5,23 @@ import conversions from "..";
 describe("WebIDL DOMString type", () => {
     var sut = conversions["DOMString"];
 
-    it("should return `\"undefined\"` for `undefined`", function () {
+    it("should return `\"undefined\"` for `undefined`", () => {
         assert.strictEqual(sut(undefined), "undefined");
     });
 
-    it("should return `\"null\"` for `null`", function () {
+    it("should return `\"null\"` for `null`", () => {
         assert.strictEqual(sut(null), "null");
     });
 
-    it("should return `\"true\"` for `true`", function () {
+    it("should return `\"true\"` for `true`", () => {
         assert.strictEqual(sut(true), "true");
     });
 
-    it("should return `\"false\"` for `false`", function () {
+    it("should return `\"false\"` for `false`", () => {
         assert.strictEqual(sut(false), "false");
     });
 
-    it("should return the correct number formatting for numbers", function () {
+    it("should return the correct number formatting for numbers", () => {
         assert.strictEqual(sut(NaN), "NaN");
         assert.strictEqual(sut(+0), "0");
         assert.strictEqual(sut(-0), "0");
@@ -31,19 +31,19 @@ describe("WebIDL DOMString type", () => {
         assert.strictEqual(sut(-10), "-10");
     });
 
-    it("should return the input for a string", function () {
+    it("should return the input for a string", () => {
         assert.strictEqual(sut(""), "");
         assert.strictEqual(sut("whee"), "whee");
     });
 
-    it("should throw a TypeError for a symbol", function () {
-        assert.throws(function () {
+    it("should throw a TypeError for a symbol", () => {
+        assert.throws(() => {
             sut(new Symbol());
         }, TypeError);
     });
 
-    it("should prefer toString to valueOf on objects", function () {
-        var o = { valueOf: function () { return 5; }, toString: function () { return "foo"; } };
+    it("should prefer toString to valueOf on objects", () => {
+        var o = { valueOf() { return 5; }, toString() { return "foo"; } };
         assert.strictEqual(sut(o), "foo");
     });
 });
